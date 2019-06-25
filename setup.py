d96797d9ee449dfe611b9d46d2cb2408338997c4
Paul K George <paulkottalil20@gmail.com>
2019-06-25 12:17:59 +0530

    corrected setup.py

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setup(
   data_files=[('testOutput', ['tests/testOutput.json']), ('readme', ['readme.md']), ('requirements', ['requirements.txt'])],
   author = 'Suyash Behera',
   author_email = 'sne9x@outlook.com',
-  url = 'https://github.com/Suyash458/WiktionaryParser', 
-  download_url = 'https://github.com/Suyash458/WiktionaryParser/archive/master.zip', 
+  url = 'https://github.com/paulkg4u/WiktionaryParser', 
+  download_url = 'https://github.com/paulkg4u/WiktionaryParser/archive/master.zip', 
   keywords = ['Parser', 'Wiktionary'],
   install_requires = ['beautifulsoup4','requests'],
   classifiers=[
